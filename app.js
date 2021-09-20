ba05bf23af906f73fced25f2fbe7e8175ddd0e79
jinwook-song <wlsdnr129@naver.com>
2021-09-20 22:38:33 +0900

    Class, Example

--- app.js
+++ app.js
@@ -1,61 +1,33 @@
-///////////////////// Basic Class /////////////////////
-
-class User {
-  constructor({ username, lastName, email, password }) {
-    this.username = username;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
+class Counter {
+  constructor({ initialNumber = 0, counterId, plusId, minusId }) {
+    this.count = initialNumber;
+    this.counter = document.getElementById(counterId);
+    this.counter.innerText = initialNumber;
+    this.plusBtn = document.getElementById(plusId);
+    this.minusBtn = document.getElementById(minusId);
+    this.addEventListners();
   }
-  sayHello() {
-    console.log(`Hello, my name is ${this.username}`);
-  }
-  getProfile() {
-    console.log(`${this.username} ${this.email} ${this.password}`);
-  }
-  updatePassword(newPassword, currentPassword) {
-    if (currentPassword === this.password) {
-      this.password = newPassword;
-      console.log("Password updated!");
-    } else {
-      console.log("Can't change password.");
-    }
-  }
+  addEventListners = () => {
+    this.plusBtn.addEventListener("click", this.increase);
+    this.minusBtn.addEventListener("click", this.decrease);
+  };
+  increase = () => {
+    this.count += 1;
+    this.repaintCount();
+  };
+  decrease = () => {
+    this.count -= 1;
+    this.repaintCount();
+  };
+  repaintCount = () => {
+    this.counter.innerText = this.count;
+  };
 }
 
-const user1 = new User({
-  username: "Jinwook",
-  lastName: "Song",
-  email: "wlsdnr129@naver.com",
-  password: "123",
+new Counter({ counterId: "count", plusId: "plus", minusId: "minus" });
+new Counter({
+  initialNumber: 540,
+  counterId: "count2",
+  plusId: "plus2",
+  minusId: "minus2",
 });
-
-user1.updatePassword("he123adf", "132");
-user1.getProfile();
-user1.updatePassword("he123adf", "123");
-user1.getProfile();
-
-///////////////////// Extending Class /////////////////////
-
-class Admin extends User {
-  constructor({ username, lastName, email, password, superAdmin, isActive }) {
-    // call the User constructure
-    super({ username, lastName, email, password });
-    this.superAdmin = superAdmin;
-    this.isActive = isActive;
-  }
-  deleteWebsite() {
-    console.log("Deleting the whole website...");
-  }
-}
-
-const admin1 = new Admin({
-  username: "Jinwook",
-  lastName: "Song",
-  email: "wlsdnr129@naver.com",
-  password: "123",
-  superAdmin: true,
-  isActive: true,
-});
-
-admin1.deleteWebsite();
