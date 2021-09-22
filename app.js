05ba277cf92960fc1f3443cf192e6754f25eab72
jinwook-song <wlsdnr129@naver.com>
2021-09-23 07:36:29 +0900

    HTML Fragments

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-class Counter {
-  constructor({ initialNumber = 0, counterId, plusId, minusId }) {
-    this.count = initialNumber;
-    this.counter = document.getElementById(counterId);
-    this.counter.innerText = initialNumber;
-    this.plusBtn = document.getElementById(plusId);
-    this.minusBtn = document.getElementById(minusId);
-    this.addEventListners();
-  }
-  addEventListners = () => {
-    this.plusBtn.addEventListener("click", this.increase);
-    this.minusBtn.addEventListener("click", this.decrease);
-  };
-  increase = () => {
-    this.count += 1;
-    this.repaintCount();
-  };
-  decrease = () => {
-    this.count -= 1;
-    this.repaintCount();
-  };
-  repaintCount = () => {
-    this.counter.innerText = this.count;
-  };
-}
+const wrapper = document.querySelector(".wrapper");
 
-new Counter({ counterId: "count", plusId: "plus", minusId: "minus" });
-new Counter({
-  initialNumber: 540,
-  counterId: "count2",
-  plusId: "plus2",
-  minusId: "minus2",
-});
+const addWelcome = () => {
+  const div = `
+    <div class="hello">
+      <h1 class="title">Hello~</h1>
+    </div>
+  `;
+  wrapper.innerHTML = div;
+};
+
+setTimeout(addWelcome, 2000);
